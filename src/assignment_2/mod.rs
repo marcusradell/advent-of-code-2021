fd0a470be6708a213f48ba1feba2cd611c090481
Marcus Rådell <marcus@radell.net>
2021-12-02 12:18:46 +0100

    assignment 2

---
+++ src/assignment_2/mod.rs
@@ -0,0 +1,57 @@
+use std::error;
+
+#[derive(Debug)]
+struct Acc {
+    count: i64,
+    vals: Vec<i64>,
+}
+
+pub fn run() -> Result<(), Box<dyn error::Error>> {
+    let mut input = include_str!("input.txt")
+        .split('\n')
+        .map(str::parse::<i64>)
+        .map(Result::unwrap);
+
+    let initial_acc = Acc {
+        count: 0,
+        vals: vec![],
+    };
+
+    let result = input.fold(initial_acc, |acc, val| {
+        if acc.vals.len() < 3 {
+            let mut vals = acc.vals;
+            vals.insert(0, val);
+
+            return Acc {
+                count: acc.count,
+                vals,
+            };
+        }
+
+        let mut vals = acc.vals;
+
+        if vals.len() > 3 {
+            vals = vals[..3].into();
+        }
+
+        vals.insert(0, val);
+
+        let prev_sum = vals[1] + vals[2] + vals[3];
+        let next_sum = vals[0] + vals[1] + vals[2];
+
+        let mut count = acc.count;
+
+        dbg!(prev_sum);
+        dbg!(next_sum);
+
+        if prev_sum < next_sum {
+            count += 1;
+        }
+
+        Acc { count, vals }
+    });
+
+    dbg!(result);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::error;
 
 mod assignment_1;
-
+mod assignment_2;
 fn main() -> Result<(), Box<dyn error::Error>> {
-    assignment_1::run().unwrap();
+    assignment_2::run().unwrap();
 
     Ok(())
 }
